dda155c747ad958d0d06848ec069df081ba303e7
art0007i <art0007i@gmail.com>
2025-09-24 21:25:29 +0200

    fix import animj crashing game

--- CustomInspectors/CustomInspectors.cs
+++ CustomInspectors/CustomInspectors.cs
@@ -1,12 +1,14 @@
+using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.Store;
 using HarmonyLib;
 using ResoniteModLoader;
-using System;
-using FrooxEngine.Store;
-using System.Collections.Generic;
-using FrooxEngine;
-using System.IO;
-using Elements.Core;
 using SpecialItemsLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
 
 namespace CustomInspectors
 {
@@ -14,7 +16,7 @@ namespace CustomInspectors
     {
         public override string Name => "CustomInspectors";
         public override string Author => "art0007i";
-        public override string Version => "2.1.6";
+        public override string Version => "2.2.0";
         public override string Link => "https://github.com/art0007i/CustomInspectors/";
 
         [AutoRegisterConfigKey]
@@ -22,10 +24,15 @@ namespace CustomInspectors
 
         public static ModConfiguration config;
 
+        static DynamicMethod reentrantMethod;
+
         public override void OnEngineInit()
         {
             config = GetConfiguration();
             Harmony harmony = new Harmony("me.art0007i.CustomInspectors");
+
+            var orig = AccessTools.Method(typeof(SceneInspector), "OnAttach");
+            reentrantMethod = (DynamicMethod) harmony.Patch(orig);
 
             // Register the special item.
             // The readable name will be shown to users in the inventory in this format: "Set {0}"
@@ -47,7 +54,7 @@ namespace CustomInspectors
         [HarmonyPatch(typeof(SlotHelper), "GenerateTags", new Type[] { typeof(Slot), typeof(HashSet<string>) })]
         class SlotHelper_GenerateTags_Patch
         {
-            static void Postfix(Slot slot, HashSet<string> tags)
+            public static void Postfix(Slot slot, HashSet<string> tags)
             {
                 if (slot.GetComponent<SceneInspector>() != null)
                 {
@@ -60,120 +67,155 @@ namespace CustomInspectors
         [HarmonyPatch(typeof(SceneInspector), "OnAttach")]
         class SceneInspector_OnAttach_Patch
         {
-            static bool Prefix(SceneInspector __instance)
+            public static bool Prefix(SceneInspector __instance)
             {
                 // If no favorite item has been selected spawn the default one
                 if (OurItem.Uri == null) return true;
 
-                // If the url is a record url, it needs to be converted to an asset url
-                var uri = OurItem.Uri;
-                if ( uri.Scheme == Engine.Current.Cloud.Platform.RecordScheme)
+                __instance.StartTask(async () =>
                 {
-                    var ctask = Engine.Current.Cloud.Records.GetRecordCached<Record>(uri, null);
-                    ctask.Wait();
-                    SkyFrost.Base.CloudResult<Record> cloudResult = ctask.Result;
-                    if (cloudResult.IsError)
+                    // Store the current transform in case spawning a custom inspector fails
+                    var pos = __instance.Slot.GlobalPosition;
+                    var rot = __instance.Slot.GlobalRotation;
+                    var scl = __instance.Slot.GlobalScale * 0.0005f;
+
+                    if (await SpawnCustomInspector(__instance))
+                    {
+                        await default(ToWorld);
+
+                        // apparently the reentrant method is some kind of static void DMD<DMD<>?45188956::FrooxEngine.SceneInspector::OnAttach>(FrooxEngine.SceneInspector this)
+                        // I swear it used to not be like that but whatever.
+                        if (reentrantMethod.IsStatic)
+                            reentrantMethod.Invoke(null, [__instance]);
+                        else
+                            reentrantMethod.Invoke(__instance, []);
+
+                        __instance.Slot.GlobalPosition = pos;
+                        __instance.Slot.GlobalRotation = rot;
+                        __instance.Slot.GlobalScale = scl;
+                    }
+                });
+
+                return false;
+            }
+
+            public static async Task<bool> SpawnCustomInspector(SceneInspector __instance)
+            {
+                try
+                {
+                    await default(ToBackground);
+                    // If the url is a record url, it needs to be converted to an asset url
+                    var uri = OurItem.Uri;
+                    if (uri.Scheme == Engine.Current.Cloud.Platform.RecordScheme)
+                    {
+                        var cloudResult = await Engine.Current.Cloud.Records.GetRecordCached<Record>(uri, null);
+                        if (cloudResult.IsError)
+                        {
+                            return true;
+                        }
+                        uri = new Uri(cloudResult.Entity.AssetURI);
+                    }
+
+                    // Download the favorite item
+                    var text = await Engine.Current.AssetManager.GatherAssetFile(uri, 20, null);
+                    if (text == null || !File.Exists(text))
                     {
                         return true;
                     }
-                    uri = new Uri(cloudResult.Entity.AssetURI);
-                }
 
-                // Download the favorite item
-                var ttask = Engine.Current.AssetManager.GatherAssetFile(uri, 20, null);
-                ttask.AsTask().Wait();
-                string text = ttask.Result;
-                if (text == null || !File.Exists(text))
-                {
-                    return true;
-                }
+                    // All the code under this is very implementation specific.
+                    // In this case it's actually quite complex because of how inspectors are generated.
 
-                // All the code under this is very implementation specific.
-                // In this case it's actually quite complex because of how inspectors are generated.
+                    // this is where we load the item and parse it to merge guids with refids
+                    DataTreeDictionary node = DataTreeConverter.Load(text, uri);
 
-                // this is where we load the item and parse it to merge guids with refids
-                DataTreeDictionary node = DataTreeConverter.Load(text, uri);
+                    var isNewTypes = node?.TryGetDictionary("FeatureFlags")?.TryGetNode("TypeManagement") != null;
 
-                var isNewTypes = node?.TryGetDictionary("FeatureFlags")?.TryGetNode("TypeManagement") != null;
-
-                var rootNode = node.TryGetDictionary("Object");
-                if (rootNode.TryGetDictionary("Name").TryGetNode("Data").LoadString() == "Holder")
-                {
-                    rootNode = rootNode.TryGetList("Children").Children[0] as DataTreeDictionary;
-                    node.Children["Object"] = rootNode;
-                }
-                var topLevel = rootNode.TryGetDictionary("Components").TryGetList("Data");
-                var translator = new ReferenceTranslator();
-                foreach (var dataNode in topLevel.Children)
-                {
-                    var dictNode = (dataNode as DataTreeDictionary);
-                    var typeNode = dictNode.TryGetNode("Type");
-                    var check = false;
-                    if (isNewTypes)
+                    var rootNode = node.TryGetDictionary("Object");
+                    if (rootNode.TryGetDictionary("Name").TryGetNode("Data").LoadString() == "Holder")
                     {
-                        var typeIdx = typeNode.LoadInt();
-                        var typeList = node.TryGetList("Types");
-                        if(typeList?.Count > typeIdx)
+                        rootNode = rootNode.TryGetList("Children").Children[0] as DataTreeDictionary;
+                        node.Children["Object"] = rootNode;
+                    }
+                    var topLevel = rootNode.TryGetDictionary("Components").TryGetList("Data");
+                    var translator = new ReferenceTranslator();
+                    foreach (var dataNode in topLevel.Children)
+                    {
+                        var dictNode = (dataNode as DataTreeDictionary);
+                        var typeNode = dictNode.TryGetNode("Type");
+                        var check = false;
+                        if (isNewTypes)
                         {
-                            check = __instance.World.Types.DecodeType(typeList[typeIdx].LoadString()) == typeof(SceneInspector);
+                            var typeIdx = typeNode.LoadInt();
+                            var typeList = node.TryGetList("Types");
+                            if (typeList?.Count > typeIdx)
+                            {
+                                check = __instance.World.Types.DecodeType(typeList[typeIdx].LoadString()) == typeof(SceneInspector);
+                            }
+                        }
+                        else
+                        {
+                            check = typeNode.LoadString() == typeof(SceneInspector).ToString();
+                        }
+
+                        if (check)
+                        {
+                            var dataDict = dictNode.TryGetDictionary("Data");
+
+                            // Component guid merge
+                            translator.Associate(__instance.ReferenceID, new Guid(dataDict.TryGetNode("ID").LoadString()));
+                            dataDict.Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
+
+                            __instance.ForeachSyncMember<IWorldElement>((member) =>
+                            {
+                                var guidStr = dataDict.TryGetDictionary(member.Name)?.TryGetNode("ID").LoadString();
+                                if (guidStr != null)
+                                {
+                                    // Sync member guids merge
+                                    translator.Associate(member.ReferenceID, new Guid(guidStr));
+                                    dataDict.TryGetDictionary(member.Name).Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
+                                }
+                            });
+                            break;
                         }
                     }
-                    else
+
+                    await default(ToWorld);
+                    // now time to actually load the object
+                    if (!__instance.Slot.IsDestroyed)
                     {
-                        check = typeNode.LoadString() == typeof(SceneInspector).ToString();
+                        var pos = __instance.Slot.GlobalPosition;
+                        var rot = __instance.Slot.GlobalRotation;
+                        var scl = __instance.Slot.GlobalScale * config.GetValue(KEY_INSPECTOR_SCALE);
+
+                        __instance.Slot.LoadObject(node, null, refTranslator: translator);
+                        var old = __instance.Slot.GetComponent<SceneInspector>((insp) => insp != __instance);
+
+                        var rt = AccessTools.Field(typeof(SceneInspector), "_rootText");
+                        var ct = AccessTools.Field(typeof(SceneInspector), "_componentText");
+                        var hcr = AccessTools.Field(typeof(SceneInspector), "_hierarchyContentRoot");
+                        var ccr = AccessTools.Field(typeof(SceneInspector), "_componentsContentRoot");
+
+                        (rt.GetValue(__instance) as SyncRef<Sync<string>>).Target = (rt.GetValue(old) as SyncRef<Sync<string>>).Target;
+                        (ct.GetValue(__instance) as SyncRef<Sync<string>>).Target = (ct.GetValue(old) as SyncRef<Sync<string>>).Target;
+                        (hcr.GetValue(__instance) as SyncRef<Slot>).Target = (hcr.GetValue(old) as SyncRef<Slot>).Target;
+                        (ccr.GetValue(__instance) as SyncRef<Slot>).Target = (ccr.GetValue(old) as SyncRef<Slot>).Target;
+
+                        old.Destroy(false);
+
+                        __instance.Enabled = true;
+                        __instance.Slot.GlobalPosition = pos;
+                        __instance.Slot.GlobalRotation = rot;
+                        __instance.Slot.GlobalScale = scl;
                     }
 
-                    if (check)
-                    {
-                        var dataDict = dictNode.TryGetDictionary("Data");
-
-                        // Component guid merge
-                        translator.Associate(__instance.ReferenceID, new Guid(dataDict.TryGetNode("ID").LoadString()));
-                        dataDict.Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
-
-                        __instance.ForeachSyncMember<IWorldElement>((member) =>
-                        {
-                            var guidStr = dataDict.TryGetDictionary(member.Name)?.TryGetNode("ID").LoadString();
-                            if (guidStr != null)
-                            {   
-                                // Sync member guids merge
-                                translator.Associate(member.ReferenceID, new Guid(guidStr));
-                                dataDict.TryGetDictionary(member.Name).Children["ID"] = new DataTreeValue(Guid.NewGuid().ToString());
-                            }
-                        });
-                        break;
-                    }
+                    return false;
                 }
-
-                // now time to actually load the object
-                if (!__instance.Slot.IsDestroyed)
+                catch (Exception e)
                 {
-                    var pos = __instance.Slot.GlobalPosition;
-                    var rot = __instance.Slot.GlobalRotation;
-                    var scl = __instance.Slot.GlobalScale * config.GetValue(KEY_INSPECTOR_SCALE);
-
-                    __instance.Slot.LoadObject(node, null, refTranslator: translator);
-                    var old = __instance.Slot.GetComponent<SceneInspector>((insp) => insp != __instance);
-
-                    var rt = AccessTools.Field(typeof(SceneInspector), "_rootText");
-                    var ct = AccessTools.Field(typeof(SceneInspector), "_componentText");
-                    var hcr = AccessTools.Field(typeof(SceneInspector), "_hierarchyContentRoot");
-                    var ccr = AccessTools.Field(typeof(SceneInspector), "_componentsContentRoot");
-
-                    (rt.GetValue(__instance) as SyncRef<Sync<string>>).Target = (rt.GetValue(old) as SyncRef<Sync<string>>).Target;
-                    (ct.GetValue(__instance) as SyncRef<Sync<string>>).Target = (ct.GetValue(old) as SyncRef<Sync<string>>).Target;
-                    (hcr.GetValue(__instance) as SyncRef<Slot>).Target = (hcr.GetValue(old) as SyncRef<Slot>).Target;
-                    (ccr.GetValue(__instance) as SyncRef<Slot>).Target = (ccr.GetValue(old) as SyncRef<Slot>).Target;
-
-                    old.Destroy(false);
-
-                    __instance.Enabled = true;
-                    __instance.Slot.GlobalPosition = pos;
-                    __instance.Slot.GlobalRotation = rot;
-                    __instance.Slot.GlobalScale = scl;
-
+                    Error("Failed to spawn custom inspector: " + e.ToString());
                 }
-                return false;
+                return true;
             }
         }
     }
